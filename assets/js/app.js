c9b287ce88e9ae1f9641ad8efee068f4bc22a3de
Wkatir <110741874+wkatir@users.noreply.github.com.>
2024-11-23 19:52:56 -0600

    feat: finishing index.html

--- assets/js/app.js
+++ assets/js/app.js
@@ -3,3 +3,11 @@ const header = document.querySelector('header');
 window.addEventListener("scroll", function () {
     header.classList.toggle("sticky", window.scrollY > 0);
 });
+
+let menu = document.querySelector('#menu-icon');
+let navmenu = document.querySelector('.header__navmenu');
+
+menu.onclick = () => {
+    menu.classList.toggle('bx-x');
+    navmenu.classList.toggle('active');
+}
